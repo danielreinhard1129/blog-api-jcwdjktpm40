8fd69a56fcf16a31cd6009c8db9ee4b09789d7d2
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2026-04-08 16:28:29 +0700

    feat: add login feature (#2)

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
+import { hash, verify } from "argon2";
+import jwt from "jsonwebtoken";
 import { PrismaClient, User } from "../../generated/prisma/client.js";
 import { ApiError } from "../../utils/api-error.js";
-import { hash } from "argon2";
 
 export class AuthService {
   constructor(private prisma: PrismaClient) {}
@@ -29,5 +30,27 @@ export class AuthService {
     return {
       message: "register success",
     };
+  };
+
+  login = async (body: User) => {
+    const user = await this.prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    if (!user) throw new ApiError("Invalid credentials", 400);
+
+    const isPassMatch = await verify(user.password, body.password);
+
+    if (!isPassMatch) throw new ApiError("Invalid credentials", 400);
+
+    const payload = { id: user.id, role: user.role };
+
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET!, {
+      expiresIn: "2h",
+    });
+
+    const { password, ...userWithoutPassword } = user; // remove property password
+
+    return { user: userWithoutPassword, accessToken };
   };
 }
