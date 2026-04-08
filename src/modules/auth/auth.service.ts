178578af6c922452c1170ade8b56d11d7d69e2c6
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-04-08 15:58:17 +0700

    feat: add register feature

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, User } from "../../generated/prisma/client.js";
+import { ApiError } from "../../utils/api-error.js";
+import { hash } from "argon2";
+
+export class AuthService {
+  constructor(private prisma: PrismaClient) {}
+
+  register = async (body: User) => {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (user) {
+      throw new ApiError("Email already exist", 400);
+    }
+
+    const hashedPassword = await hash(body.password);
+
+    await this.prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      message: "register success",
+    };
+  };
+}
